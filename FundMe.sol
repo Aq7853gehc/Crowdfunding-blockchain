8b3f2c0de4ceb08cc5e0f27736762447dace65ff
Aq7853gehc <aquib7583@gmail.com>
2025-01-02 16:36:13 +0530

    complete the crowdfunding test and I added to raise fund and withdraw to owner of the contract

--- FundMe.sol
+++ FundMe.sol
@@ -14,33 +14,47 @@ contract FundMe {
     address[] public fundersAdd;
     mapping(address => uint256) public addressToAmounFunded;
 
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require(msg.value.getConvert() >= minimumUSD, "Didn't send enough ETH"); // 1e18 = 1*10**18;
-        funders.push(msg.sender);
+        fundersAdd.push(msg.sender);
         addressToAmounFunded[msg.sender] =
             addressToAmounFunded[msg.sender] +
             msg.value;
     }
 
-    function withdraw() public {
-        for (uint256 i = 0; i < fundersAdd.lengt; i++) {
+    function withdraw() public onlyOwner {
+        require(msg.sender == owner, "Must be owner");
+        for (uint256 i = 0; i < fundersAdd.length; i++) {
             address funder = fundersAdd[i];
             addressToAmounFunded[funder] = 0;
         }
-        // Reset the array of funder 
-
         fundersAdd = new address[](0);
 
         /*
             withdraw ETH has three way :
             1. Transfer (2300 gas ,throws error)
+                    payable(msg.sender).transfer(address(this).balance);
             2. Send (2300 gas, return bool)
-            3. Call(forward all gas or set gas, returns bool)
+                    bool sendSuccess=payable(msg.sender).send(address(this).balance);
+                    require(sendSuccess,"Send failed");
+            3. Call(forward all gas or set gas, returns bool) use 
         */
-        // 1: Transfer 
-        payable(msg.sender).transfer(address(this).balance);
-        // send
-        payable(msg.sender).send(address(this).balance);
 
+        
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be owner");
+        _;
     }
 }
